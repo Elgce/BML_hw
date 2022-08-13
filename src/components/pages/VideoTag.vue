a1a6beb4deaf71c96c836a3bcb5e741eb63d33ee
Yunzhe Shao <2225832088@qq.com>
2022-08-13 20:01:00 +0800

    实现视频播放和简易外在进度条

--- src/components/pages/VideoTag.vue
+++ src/components/pages/VideoTag.vue
@@ -1,16 +1,30 @@
 <template>
-<span>Hello</span>
+    <span>Hello</span>
+    <video id="myVideo" width="320" height="240" controls>
+        <source src="../../assets/movie.mp4"  type="video/mp4">
+    </video>
+    <progress id="progressBar" value="0" max="100"></progress>
 </template>
 
 
 <script>
 export default{
     name: "AddTag",
+    mounted(){
+        var video = document.getElementById("myVideo");
+        var progress = document.getElementById("progressBar");
+        video.ontimeupdate = function() {
+            progress.value = 100 * video.currentTime / video.duration;
+        }
 
+    }
 }
 </script>
 
 
 <style scoped>
-
+#progressBar{
+    width: 320px;
+    height: 20px;
+}
 </style>
