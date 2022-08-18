a1e909a7eaca4e0944729f781cfdabf7e26b24e8
Jingzhou Wang <75514859+2475264333@users.noreply.github.com>
2022-08-18 19:12:34 +0800

    添加了平台说明页面
    添加了平台说明页面

---
+++ src/components/pages/PlatformIntroduction.vue
@@ -0,0 +1,27 @@
+<template>
+    <el-button>123</el-button>
+</template>
+
+
+<script>
+    export default
+    {
+        name: "PlatformIntro",
+        data()
+        {
+            return{
+                
+            };
+        },
+        methods:{
+
+        }
+    }
+
+
+</script>
+
+
+<style scoped>
+
+</style>
