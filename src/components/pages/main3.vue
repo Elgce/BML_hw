0842584379dbeb6752f5356b73b924f584e8d009
Jingzhou Wang <75514859+2475264333@users.noreply.github.com>
2022-08-22 13:08:18 +0800

    Update main3.vue

--- src/components/pages/main3.vue
+++ src/components/pages/main3.vue
@@ -94,9 +94,9 @@
                             v-if="show_btn===false"
                         >
                             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            <el-button id="add_tagGroup">添加标签组</el-button>
+                            <el-button id="add_tagGroup" @click="addMarkGroup">添加标签组</el-button>
                             <div v-for="item in hasLabel_info" :key="item" >
-                                <el-button class="labels_choice" @click="has_add_label(item)" style="width:150px;">
+                                <el-button class="labels_choice" @click="has_add_labels(item)" style="width:150px;">
                                 {{item}}
                                 </el-button>
                             </div>
@@ -379,11 +379,11 @@ import { reactive, ref } from "vue"
                     this.$router.push("/index/manage/dataset/pic/label/blank");
                 })
             },
-            has_add_label(item){
+            has_add_labels(item){
                 const data = {"name": item};
                 console.log("abd");
                 console.log(data);
-                return fetch("/api/addlabel",{
+                return fetch("/api/addlabels",{
                     method: 'POST',
                     headers: {
                         "Content-Type": "application/json",
@@ -520,6 +520,13 @@ import { reactive, ref } from "vue"
                 {
                     this.marking_date=null
                 }
+            },
+
+            
+            //添加标签组
+            addMarkGroup()
+            {
+                this.$router.push("/index/manage/dataset/pic/addtag");
             }
         }
     }
